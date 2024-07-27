5df5b05d1c207ca0bb77bf9894e89d9d291ffdd6
Zheka Grem <odinked@gmail.com>
2024-07-27 20:57:10 +0300

    add new block2

--- src/app/components/section/CourseSteps.tsx
+++ src/app/components/section/CourseSteps.tsx
@@ -3,14 +3,13 @@ import React from 'react';
 import { Button } from '../common/Button';
 import { usePortal } from '@/app/components/layout/PortalContext';
 
-
 const CourseSteps = () => {
   const { isPortalOpen, setIsPortalOpen } = usePortal();
   return (
     <section className="imgBg3 flex min-h-screen bg-cover bg-center">
       <div className="container mx-auto"></div>
     </section>
-  ); 
+  );
 };
 
 export default CourseSteps;
