b4395185bd42c4585aae92ca72e55a6870496aa0
Zheka Grem <odinked@gmail.com>
2024-07-30 12:46:59 +0300

    test

--- src/app/components/section/PhonePortfolio.tsx
+++ src/app/components/section/PhonePortfolio.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
-import PhoneCarousel from '../layout/PhoneCarousel';
+import { lazy, Suspense } from 'react';
+import Loading from '@/app/loading';
+const PhoneCarousel = lazy(() => import('@/app/components/layout/PhoneCarousel'));
 import { section_5 } from '@/db/data';
 
 const data: string = section_5;
@@ -8,8 +9,10 @@ const PhonePortfolio = () => {
   return (
     <section id="portfolio" className="pb-0">
       <div className="block text-text_2 drop-shadow-2xl text-shadow-black sm:hidden">
-        <h2 className="absolute z-10 pt-6">{data}</h2>
-        <PhoneCarousel />
+        <h2 className="absolute z-10 w-full pt-6 text-center">{data}</h2>
+        <Suspense fallback={<Loading />}>
+          <PhoneCarousel />
+        </Suspense>
       </div>
     </section>
   );
