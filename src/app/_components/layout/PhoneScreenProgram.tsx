2e2b7ed2c668200e372f22aeae7cd6fcfa5e5857
Zheka Grem <odinked@gmail.com>
2024-08-06 20:53:10 +0300

    fix photo style new video

--- src/app/_components/layout/PhoneScreenProgram.tsx
+++ src/app/_components/layout/PhoneScreenProgram.tsx
@@ -1,9 +1,10 @@
 'use client';
-import React, { useState, useRef } from 'react';
+import React, { useState } from 'react';
 import Image from 'next/image';
 import { motion, AnimatePresence } from 'framer-motion';
 
 type IndexType = number | null;
+type HandleClickButton = (index: number) => void;
 
 type InfoType = {
   id: number;
@@ -23,21 +24,12 @@ type ComponentProps = {
 };
 
 const PhoneScreenProgram: React.FC<ComponentProps> = ({ data }) => {
-  const [openIndex, setOpenIndex] = useState(data[0].id);
+  const [openIndex, setOpenIndex] = useState<IndexType>(null);
   const [isPlaying, setIsPlaying] = useState(false);
-  const videoRef = useRef<HTMLVideoElement>(null);
 
-  const handleButtonClick = (id: number) => {
-    setOpenIndex(id);
-    setIsPlaying(false);
-  };
+const handleButtonClick: HandleClickButton = (index) => {
+    setOpenIndex(openIndex === index ? null : index);}
 
-  const handlePlayClick = () => {
-    setIsPlaying(true);
-    if (videoRef.current) {
-      videoRef.current.play();
-    }
-  };
   return (
     <div className="section container">
       <div className="flex flex-col content-center gap-12 lg:flex-row">
@@ -115,34 +107,9 @@ const PhoneScreenProgram: React.FC<ComponentProps> = ({ data }) => {
                                 <div className="relative">
                                   {item.content.video ? (
                                     <>
-                                      {!isPlaying && (
-                                        <>
-                                          <Image
-                                            className="h-full w-full rounded-lg object-cover shadow-2xl"
-                                            src={item.content.img}
-                                            alt={item.content.img_alt}
-                                            width={640}
-                                            height={360}
-                                            priority={true}
-                                          />
-                                          <button
-                                            className="absolute inset-0 flex items-center justify-center text-black"
-                                            onClick={handlePlayClick}>
-                                            <div className="flex items-center justify-center rounded-full bg-white bg-opacity-70 p-3 shadow-[0_0_10px_rgba(255,255,255,0.7)]">
-                                              <svg
-                                                className="h-8 w-8 sm:h-12 sm:w-12 md:h-16 md:w-16"
-                                                viewBox="0 0 24 24"
-                                                fill="currentColor">
-                                                <path d="M8 5v14l11-7z" />
-                                              </svg>
-                                            </div>
-                                          </button>
-                                        </>
-                                      )}
                                       <video
-                                        ref={videoRef}
-                                        className={`h-full w-full rounded-lg object-cover shadow-2xl ${isPlaying ? 'block' : 'hidden'}`}
-                                        controls={isPlaying}>
+                                        className={`h-full w-full rounded-lg object-cover shadow-2xl`}
+                                        controls>
                                         <source src={item.content.video} type="video/mp4" />
                                         Your browser does not support the video tag.
                                       </video>
