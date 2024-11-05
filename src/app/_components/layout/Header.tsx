a20dfd832730493175587f939b0a3355221a29b7
Zheka Grem <odinked@gmail.com>
2024-11-05 18:42:52 +0200

    fix(ui) change size font

--- src/app/_components/layout/Header.tsx
+++ src/app/_components/layout/Header.tsx
@@ -33,7 +33,7 @@ const Header = () => {
   };
 
   return (
-    <header className={`absolute left-0 top-0 z-50 w-full text-text_header`}>
+    <header className={`absolute left-0 top-0 z-50 w-full text-lg text-text_header`}>
       <nav className="start-0 top-0 z-20 border-b bg-background_header">
         <div className="mx-auto flex max-w-screen-xl flex-wrap items-center justify-between p-4">
           <Link href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
@@ -62,7 +62,7 @@ const Header = () => {
           </div>
           <div
             className={`w-full items-center justify-between lg:order-1 lg:flex lg:w-auto ${
-              burgerMenu ? 'hidden text-sm' : 'block text-center text-3xl'
+              burgerMenu ? 'hidden' : 'block text-center text-3xl'
             }`}>
             <ul className="mt-4 flex flex-col rounded-lg border border-gray-100 p-4 font-bold lg:mt-0 lg:flex-row lg:space-x-8 lg:border-0 lg:p-0">
               {dataLink.map((list) => (
