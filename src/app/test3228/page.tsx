9b35c8fa60bb4d483216cdcf476afe5e64191a19
Zheka Grem <odinked@gmail.com>
2024-11-20 15:56:00 +0200

    test img

--- src/app/test3228/page.tsx
+++ src/app/test3228/page.tsx
@@ -2,19 +2,7 @@ import React from 'react';
 import Image from 'next/image';
 
 function page() {
-  return (
-    <div className="relative h-screen w-full">
-      <Image
-        className="object-cover"
-        loading="lazy"
-        src="../assets/img/google.webp"
-        alt="Портфоліо студентів, фотошкола, школа фотографії, курси фотографа"
-        fill
-        quality={100}
-        sizes="100vw"
-      />
-    </div>
-  );
+  return <div className="imgBgtest relative h-screen w-full"></div>;
 }
 
 export default page;
