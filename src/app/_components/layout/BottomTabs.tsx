89b79bafdd13c112e0c674cba59cdaef2731af23
Zheka Grem <odinked@gmail.com>
2025-03-27 10:58:46 +0200

    new info

--- src/app/_components/layout/BottomTabs.tsx
+++ src/app/_components/layout/BottomTabs.tsx
@@ -7,7 +7,7 @@ import { useSearchParams } from 'next/navigation';
 const tabs = [
   { name: 'Швидкий старт', href: '#faststart', id: 'faststart' },
   { name: 'PRO Світло', href: '#prosvitlo', id: 'prosvitlo' },
-  { name: 'Індивідуальна школа', href: '#school', id: 'school' },
+  // { name: 'Індивідуальна школа', href: '#school', id: 'school' },
 ];
 
 const BottomTabs = () => {
@@ -28,19 +28,19 @@ const BottomTabs = () => {
   return (
     <>
       {/* prettier-ignore */}
-      <div className="sticky bottom-4 left-1/2  z-10 w-full max-w-lg px-3 lg:-translate-x-1/2  ">
+      <div className="sticky bottom-4 left-1/2  z-10 w-full max-w-xs px-3 lg:-translate-x-1/2  ">
       <nav className="rounded-full bg-background_btn_burger animate-tilt-shaking ">
         <div className="flex max-w-screen-xl flex-wrap items-center justify-between p-2">
           <ul className="flex w-full justify-between">
             {tabs.map((tab, id) => (
-              <li key={id} className="font-bold text-white">
+              <li key={id} className="font-bold text-white hover:shadow-xl ">
                 <button onClick={() => handleVariantChange(tab.id)}>
                   <Link
                     href={tab.href}
-                    className={`block rounded-2xl p-1 text-center text-xs font-medium shadow-md transition duration-300 ease-in-out md:p-2 ${
+                    className={`block rounded-2xl p-1 text-center text-xs font-medium shadow-lg transition duration-300 ease-in-out md:p-2 ${
                       variantId  === tab.id
                         ? 'cursor-pointer rounded-full bg-text_2 text-background_btn_burger'
-                        : 'transition hover:bg-background_btn hover:text-black active:scale-95'
+                        : 'transition hover:bg-background_btn hover:text-black active:scale-95 '
                     }`}>
                     <span className="font-semibold md:text-base">{tab.name}</span>
                   </Link>
