b59567042e25f324465ce90eb471e63ee50bfef0
Jeka <odinked@gmail.com>
2025-06-13 19:30:02 +0300

    👨‍🏫

--- src/app/_components/layout/BottomTabs.tsx
+++ src/app/_components/layout/BottomTabs.tsx
@@ -8,7 +8,7 @@ import { motion } from 'framer-motion';
 const tabs = [
   { name: 'Швидкий старт', href: '/#faststart', id: 'faststart', isExternal: true },
   { name: 'PRO Світло', href: '/#prosvitlo', id: 'prosvitlo', isExternal: true },
-  { name: 'Про Автора', href: '/mentor#author', id: 'mentor', isExternal: true },
+  { name: '👨‍🏫', href: '/mentor#author', id: 'mentor', isExternal: true },
 ];
 
 const BottomTabs = () => {
