962953b12db4a3c5cb36dedeb2b06507dc03d51e
Jeka <odinked@gmail.com>
2025-06-18 10:59:25 +0300

    ui text

--- src/app/_components/layout/BottomTabs.tsx
+++ src/app/_components/layout/BottomTabs.tsx
@@ -8,7 +8,7 @@ import { motion } from 'framer-motion';
 const tabs = [
   { name: 'Швидкий старт', href: '/#faststart', id: 'faststart', isExternal: true },
   { name: 'PRO Світло', href: '/#prosvitlo', id: 'prosvitlo', isExternal: true },
-  { name: 'ВІДГУКИ', href: '/feedback', id: 'feedback', isExternal: true },
+  { name: 'Відгуки', href: '/feedback', id: 'feedback', isExternal: true },
 ];
 
 const BottomTabs = () => {
