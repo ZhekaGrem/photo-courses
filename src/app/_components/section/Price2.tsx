6205ab39173350299b2a35d0e31e7dfcd186aa29
Jeka <odinked@gmail.com>
2025-06-18 17:40:25 +0300

    colors9 section

--- src/app/_components/section/Price2.tsx
+++ src/app/_components/section/Price2.tsx
@@ -43,9 +43,9 @@ const Price2 = () => {
     setIsPortalOpen(true);
   };
   return (
-    <section className="bg-background_section_6">
+    <section className="bg-neon_navy">
       <div className="section container mx-auto">
-        <h2 className="mb-12 text-center text-text_2">ВАРТІСТЬ НАВЧАННЯ</h2>
+        <h2 className="mb-12 text-center text-cloud_dancer">ВАРТІСТЬ НАВЧАННЯ</h2>
 
         <div
           id="price"
@@ -56,7 +56,7 @@ const Price2 = () => {
               initial={{ opacity: 0, y: 50 }}
               animate={{ opacity: 1, y: 0 }}
               transition={{ duration: 0.5, delay: index * 0.1 }}
-              className="relative mx-auto max-w-[361px] overflow-hidden rounded-2xl bg-white shadow-2xl">
+              className="relative mx-auto max-w-[361px] overflow-hidden rounded-2xl bg-cloud_dancer shadow-2xl">
               {plan.content.discount && (
                 <div className="absolute right-0 top-0 rounded-bl-lg bg-red-500 px-4 py-1 font-bold text-white">
                   Знижка {plan.content.discount}%
@@ -81,7 +81,7 @@ const Price2 = () => {
                   <Button
                     text="Замовити"
                     onClick={() => openPortal(plan.content.title)}
-                    className="w-full max-w-60 rounded-lg bg-background_section_6 px-4 py-3 font-bold text-text_2 drop-shadow-xl transition-colors hover:bg-opacity-90">
+                    className="w-full max-w-60 rounded-lg bg-pageant_blue px-4 py-3 font-bold text-cloud_dancer drop-shadow-xl transition-colors hover:bg-opacity-90">
                     Замовити
                   </Button>
                 </div>
