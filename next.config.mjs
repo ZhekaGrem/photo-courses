3463fdacc56d89de81aa9cf0163d1abdd8c133fd
Jeka <odinked@gmail.com>
2025-06-28 14:13:16 +0300

    change size img

--- next.config.mjs
+++ next.config.mjs
@@ -21,8 +21,7 @@ const nextConfig = {
         pathname: '/**',
       },
     ],
-    minimumCacheTTL: 60 * 60 * 24 * 30,
-    // minimumCacheTTL: 60 * 60 * 24 * 7, // 7 дній для проду
+    minimumCacheTTL: 60 * 60 * 24 * 7, // 7 дній для проду
     deviceSizes: [640, 768, 1024, 1280, 1600],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
 
